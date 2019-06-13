afee9eb297ece6c6c7fa5398d1860e72d3bee453
Rob <rklaumbach@gmail.com>
2019-06-12 22:35:27 -0400

    added login UI

--- app/src/main/java/com/example/clack/MainActivity.kt
+++ app/src/main/java/com/example/clack/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.clack
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.FloatingActionButton
 import android.support.design.widget.Snackbar
@@ -41,6 +42,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun loginBtnNavClicked(view: View) {
+        val loginIntent = Intent(this, LoginActivity::class.java)
+        startActivity(loginIntent)
 
     }
 
