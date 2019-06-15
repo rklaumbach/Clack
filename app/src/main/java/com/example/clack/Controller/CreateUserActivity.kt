d957ae71d38a31b8236332d19c1e0c20715d7b5b
Rob <rklaumbach@gmail.com>
2019-06-15 14:03:02 -0400

    polished: added spinner and toast error messages, broadcast manager

--- app/src/main/java/com/example/clack/Controller/CreateUserActivity.kt
+++ app/src/main/java/com/example/clack/Controller/CreateUserActivity.kt
@@ -1,12 +1,16 @@
 package com.example.clack.Controller
 
+import android.content.Intent
 import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.content.LocalBroadcastManager
 import android.view.View
+import android.widget.Toast
 import com.example.clack.R
 import com.example.clack.Services.AuthService
 import com.example.clack.Services.UserDataService
+import com.example.clack.Utilities.BROADCAST_USER_DATA_CHANGE
 import kotlinx.android.synthetic.main.activity_create_user.*
 import java.util.*
 
@@ -18,6 +22,7 @@ class CreateUserActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_user)
+        createSpinner.visibility = View.INVISIBLE
     }
 
     fun generateUserAvatar(view: View){
@@ -55,28 +60,58 @@ class CreateUserActivity : AppCompatActivity() {
     }
 
     fun createUserClicked (view: View){
+        enableSpinner(true)
         val username = createUsernameText.text.toString()
         val email = createEmailText.text.toString()
         val password = createPasswordText.text.toString()
+
+        if (username.isEmpty() || email.isEmpty() || password.isEmpty()){
+            Toast.makeText(this, "Make sure username, email and password are filled in.",Toast.LENGTH_LONG).show()
+            enableSpinner(false)
+            return
+        }
+
         AuthService.registerUser(this, email, password){registerSuccess ->
             if (registerSuccess) {
                 AuthService.loginUser(this, email, password) {loginSuccess ->
                     if (loginSuccess) {
                         AuthService.createUser(this, username, email, userAvatar, avatarColor){createSuccess ->
                             if(createSuccess){
-                                println(UserDataService.avatarName)
-                                println(UserDataService.avatarColor)
-                                println(UserDataService.name)
-                                println(UserDataService.email)
-
+                                val userDataChange = Intent(BROADCAST_USER_DATA_CHANGE)
+                                LocalBroadcastManager.getInstance(this).sendBroadcast(userDataChange)
+                                enableSpinner(false)
                                 finish()
                             }
+                            else{
+                                errorToast()
+                            }
                         }
+                    } else{
+                        errorToast()
                     }
                 }
+            } else{
+                errorToast()
             }
         }
 
+    }
+
+    fun errorToast(){
+        Toast.makeText(this, "Something went wrong, try again",
+            Toast.LENGTH_LONG).show()
+        enableSpinner(false)
+    }
+
+    fun enableSpinner(enable: Boolean){
+        if (enable){
+            createSpinner.visibility = View.VISIBLE
+        } else{
+            createSpinner.visibility = View.INVISIBLE
+        }
+        createUserBtn.isEnabled = !enable
+        createAvatarImageView.isEnabled = !enable
+        backgroundColorBtn.isEnabled = !enable
     }
 
 
--- app/src/main/java/com/example/clack/Utilities/Constants.kt
+++ app/src/main/java/com/example/clack/Utilities/Constants.kt
@@ -1,6 +1,11 @@
 package com.example.clack.Utilities
 
+import android.content.BroadcastReceiver
+
 const val BASE_URL = "https://clackityclack.herokuapp.com/v1/"
 const val URL_REGISTER = "${BASE_URL}account/register"
 const val URL_LOGIN = "${BASE_URL}account/login"
 const val URL_CREATE_USER = "${BASE_URL}user/add"
+
+//Broadcast Constants
+const val BROADCAST_USER_DATA_CHANGE = "BROADCAST_USER_DATA_CHANGE"
